b64d80f14818b39a7f2b2f836516954bcd724fe0
BeskonchinRV <BeskonchinRV@314D-4>
2023-03-13 10:47:17 +0700

    Добавьте файлы проекта.

---
+++ ArraySortWithMenu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArraySortWithMenu
+{
+    class Menu
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
